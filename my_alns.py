6d4f4a404e0bd6dcce6207bdcc36f955e78c1bb2
jdelvalle2002 <63021540+jdelvalle2002@users.noreply.github.com>
2023-12-04 03:19:58 -0300

    iniciando implementación con grafo

--- my_alns.py
+++ my_alns.py
@@ -11,6 +11,32 @@ import math
 
 # la idea es que el grafo represente la red de distribucion de una empresa, donde los nodos
 
-demandas = random.sample(range(1, 100), 10)
+demandas = random.sample(range(10, 100), 10)
 
-print(demandas)
+SEED = 1234
+random.seed(SEED)
+# generamos el grafo de los clientes
+G = nx.Graph()
+G.add_nodes_from(range(10))
+for i in range(10):
+    for j in range(10):
+        if i != j:
+            decision = random.binomial(1, 0.7, seed=SEED)
+            if decision == 1:
+                G.add_edge(i, j, weight=random.randint(1, 10, seed=SEED))
+
+# añadimos el nodo fuente
+
+G.add_node(10)
+# lo conectamos con la red mediante 3 aristas
+utilizados = []
+for l in range(3):
+    k = random.randint(0, 9, seed=SEED)
+    utilizados.append(k)
+    G.add_edge(10, k, weight=random.randint(1, 10, seed=SEED))
+
+# asignamos demandas a los nodos
+for i in range(10):
+    G.nodes[i]['demand'] = demandas[i]
+
+
