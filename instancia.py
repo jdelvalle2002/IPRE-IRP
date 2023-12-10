666f5dde0fb66f1339d087e24e98af7ee954f4ce
Hanfud4 <pmellam@uc.cl>
2023-12-10 19:04:42 -0300

    instancia y simulacion

--- instancia.py
+++ instancia.py
@@ -14,63 +14,103 @@ from funciones import read_data, calcular_distancia, calcular_matriz_dist, calcu
 random.seed(42)
 np.random.seed(42)
 
-ubis, cap_tpte, info_locales = read_data('IRP1.xlsx')
+def crear_grafo_inicial(archivo = 'IRP1.xlsx', plot = False):
+    ubis, cap_tpte, info_locales = read_data(archivo)
 
-G = nx.DiGraph()
-color_nodos = []
-color_arcos = []
-ancho_edges = []
+    G = nx.DiGraph()
+    color_nodos = []
+    color_arcos = []
+    ancho_edges = []
 
-for local in info_locales.itertuples():
-    # print(local.i)
-    G.add_node(f"N_{local.i}", Inv = local.I, Up = local.U, Low = local.L, Prod = local.r, h = local.h,
-        coord_x = local.X, coord_y = local.Y, pos = (local.X, local.Y))
-    if local.i != 0:
-        color_nodos.append('blue') 
-    else:
-        color_nodos.append('red')
+    for local in info_locales.itertuples():
+        G.add_node(f"N_{local.i}", Inv = local.I, Up = local.U, Low = local.L, Prod = local.r, h = local.h,
+            coord_x = local.X, coord_y = local.Y, pos = (local.X, local.Y))
+        if local.i != 0:
+            color_nodos.append('blue') 
+        else:
+            color_nodos.append('red')
+        
+    for local in G.nodes():
+        for nodo in G.nodes():
+            if local != nodo:
+                dist = calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos'])
+                G.add_edge(local, nodo, weight=dist)
+                if local != 'N_0' and nodo != 'N_0':
+                    ancho_edges.append(0.25)
+                    color_arcos.append('gray')
+                elif local == 'N_0' or nodo == 'N_0':
+                    ancho_edges.append(1)
+                    color_arcos.append('black')
+    if plot:
+        plt.figure(figsize=(5,5))
+        pos=nx.get_node_attributes(G,'pos')
+        nx.draw(G, pos=pos, with_labels=True, node_size=18, font_size=15, node_color=color_nodos, width=ancho_edges, edge_color=color_arcos)
+        plt.show()
 
-e=0
+    return G, ubis, cap_tpte, info_locales
+
+
+
+
+
+
+
+# ubis, cap_tpte, info_locales = read_data('IRP1.xlsx')
+
+# G = nx.DiGraph()
+# color_nodos = []
+# color_arcos = []
+# ancho_edges = []
+
+# for local in info_locales.itertuples():
+#     # print(local.i)
+#     G.add_node(f"N_{local.i}", Inv = local.I, Up = local.U, Low = local.L, Prod = local.r, h = local.h,
+#         coord_x = local.X, coord_y = local.Y, pos = (local.X, local.Y))
+#     if local.i != 0:
+#         color_nodos.append('blue') 
+#     else:
+#         color_nodos.append('red')
+
+# e=0
+# # for local in G.nodes():
+# #     for nodo in G.nodes():
+# #         if local != nodo:
+# #             decision = 1
+# #             if decision == 1 and local != 'N_0' and nodo != 'N_0':
+# #                 dist = calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos'])
+# #                 G.add_edge(local, nodo, weight=dist)
+# #                 e +=1
+# #                 # ancho_edges.append(dist/1000)
+# #                 ancho_edges.append(0.25)
+# #                 color_arcos.append('gray')
+# #             elif local == 'N_0' or nodo == 'N_0':
+# #                 G.add_edge(local, nodo, weight=calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos']))
+# #                 e +=1
+# #                 ancho_edges.append(1)
+# #                 color_arcos.append('black')
+# #             else:
+# #                 ancho_edges.append(0)
+    
 # for local in G.nodes():
 #     for nodo in G.nodes():
 #         if local != nodo:
-#             decision = 1
-#             if decision == 1 and local != 'N_0' and nodo != 'N_0':
-#                 dist = calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos'])
-#                 G.add_edge(local, nodo, weight=dist)
-#                 e +=1
-#                 # ancho_edges.append(dist/1000)
+#             dist = calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos'])
+#             G.add_edge(local, nodo, weight=dist)
+#             e +=1
+#             if local != 'N_0' and nodo != 'N_0':
 #                 ancho_edges.append(0.25)
 #                 color_arcos.append('gray')
 #             elif local == 'N_0' or nodo == 'N_0':
-#                 G.add_edge(local, nodo, weight=calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos']))
-#                 e +=1
 #                 ancho_edges.append(1)
 #                 color_arcos.append('black')
-#             else:
-#                 ancho_edges.append(0)
-    
-for local in G.nodes():
-    for nodo in G.nodes():
-        if local != nodo:
-            dist = calcular_distancia(G.nodes[local]['pos'], G.nodes[nodo]['pos'])
-            G.add_edge(local, nodo, weight=dist)
-            e +=1
-            if local != 'N_0' and nodo != 'N_0':
-                ancho_edges.append(0.25)
-                color_arcos.append('gray')
-            elif local == 'N_0' or nodo == 'N_0':
-                ancho_edges.append(1)
-                color_arcos.append('black')
 
-plt.figure(figsize=(5,5))
+# plt.figure(figsize=(5,5))
 
-pos=nx.get_node_attributes(G,'pos')
-# nx.draw(G, pos=pos, with_labels=True, node_size=18, font_size=15, node_color=color_nodos, width=ancho_edges, edge_color=color_arcos)
-# plt.show()
-# print(G.nodes)
-# print(color_arcos, color_nodos, ancho_edges)
-# for edge in G.edges():
-#     print(edge, G.edges[edge]['weight'])
-
+# pos=nx.get_node_attributes(G,'pos')
+# # nx.draw(G, pos=pos, with_labels=True, node_size=18, font_size=15, node_color=color_nodos, width=ancho_edges, edge_color=color_arcos)
+# # plt.show()
+# # print(G.nodes)
+# # print(color_arcos, color_nodos, ancho_edges)
+# # for edge in G.edges():
+# #     print(edge, G.edges[edge]['weight'])
 
